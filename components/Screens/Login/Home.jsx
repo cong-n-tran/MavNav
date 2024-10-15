890ecc34317e3ce18c057b2b7ffa8e44c609ab1b
cong-n-tran <127175459+cong-n-tran@users.noreply.github.com>
2024-10-14 21:18:12 -0500

    added tailwind css and added quick designs to login page

--- components/Screens/Login/Home.jsx
+++ components/Screens/Login/Home.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Pressable, Button } from 'react-native';
+import { styled } from 'nativewind';
+
+import { StyledView, StyledText } from '../../Style/Style';
+
+// const StyledView = styled(View);
+// const StyledText = styled(Text);
 
 const Home = ({ navigation }) => {
   return (
-    <View>
-      <Text>
-          Home Screen
-      </Text>
-      <Button
-        title="Go to Login"
+    <StyledView className="flex-1 justify-center items-center bg-blue-50 p-6">
+
+      <StyledText className="text-4xl font-bold text-blue-800 mb-4">
+        MavNav
+      </StyledText>
+  
+      <StyledText className="text-lg text-gray-700 text-center mb-6">
+        Welcome to our app! We created this platform to help users easily navigate 
+        through their daily tasks. It's designed to simplify your routine and make 
+        life more efficient. Log in to get started!
+      </StyledText>
+
+      <Pressable
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         onPress={() => navigation.navigate('Login')}
-      />
-    </View>
+        android_ripple={{ color: '#fff' }} // Adds a ripple effect on Android
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? 'rgb(37 99 235)' : 'rgb(59 130 246)', // Change color when pressed
+          },
+        ]}
+      >
+        <StyledText className="text-white font-bold">
+          Login
+        </StyledText>
+      </Pressable>
+    </StyledView> 
   );
 };
 
