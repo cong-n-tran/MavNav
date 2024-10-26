d1fc55166cb1f916244fea357121040e3949f6ae
cong-n-tran <127175459+cong-n-tran@users.noreply.github.com>
2024-10-26 00:42:07 -0500

    add entities for profile and started doing stuff on settings

--- components/Style/Style.jsx
+++ components/Style/Style.jsx
@@ -1,4 +1,13 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { styled } from "nativewind";
 export const StyledView = styled(View);
 export const StyledText = styled(Text);
+export const StyledViewProfile = styled(View, 'flex-1 p-6 bg-blue-50');
+
+export const StyledTextProfile = styled(Text, 'text-blue-800 text-base');
+
+export const ProfileContainer = styled(View, 'flex-row justify-between mb-4');
+
+export const ProfileColumn = styled(View, 'flex-1 mx-2');
+
+export const LogoutButton = styled(Pressable, 'bg-blue-500 rounded-full p-3 items-center');
