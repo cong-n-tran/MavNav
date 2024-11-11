2ccfe43a09295b561552c0eef8b5dbbbd9e48279
cong-n-tran <127175459+cong-n-tran@users.noreply.github.com>
2024-11-10 18:30:38 -0600

    added more locations

--- components/Screens/MainPage/Map/Locations.jsx
+++ components/Screens/MainPage/Map/Locations.jsx
@@ -1,6 +1,11 @@
-// storing all given locatiosn
+// use this website to find lat and long: https://www.latlong.net/
+
+// storing all given locations
 export const locations = [
     { label: "MAC", value: { latitude: 32.731841, longitude: -97.116840 } },
     { label: "Geoscience Building", value: { latitude: 32.731812, longitude: -97.113861 } },
-    { label: "Building C", value: { latitude: 32.7330, longitude: -97.1075 } },
+    { label: "Science and Engineering Innovation and Research Building (SEIR)", value: { latitude: 32.728036, longitude: -97.113269 } },
+    { label: "College of Business", value: { latitude: 32.729716, longitude: -97.110664 } },
+    { label: "Wolfe Hall", value: { latitude: 32.731697, longitude: -97.112533 } },
+    { label: "Engineering Research Building (ERB)", value: { latitude: 32.733331, longitude: -97.113498 } },
   ];
